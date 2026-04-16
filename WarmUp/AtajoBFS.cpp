b8bf1b168f0effa9362c35b5989e4fac72161a00
dyann <vcordobacorrales@gmail.com>
2026-04-16 08:17:25 -0500

    Update and rename ejer.cpp to AtajoBFS.cpp

---
+++ WarmUp/AtajoBFS.cpp
@@ -0,0 +1,53 @@
+/*
+Calcular la distancia minima del nodo a al nodo b, graph dirijido
+*/
+
+constexpr int mxN = 1e5 + 100;
+vector<int> G[mxN];
+vector<bool> vis(mxN);
+vector<int> dist(mxN);
+
+void bfs(int u) {
+  queue<int> q;
+  q.push(u);
+  vis[u] = 1;
+  dist[u] = 0;
+  while (sz(q)) {
+    int v = q.front();
+    q.pop();
+    each(x, G[v]) {
+      if (!vis[x]) {
+        vis[x] = 1;
+        dist[x] = dist[v] + 1;
+        q.push(x);
+      }
+    }
+  }
+}
+
+main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m, a, b, u, v;
+    cin >> n >> m >> a >> b;
+    a--, b--;
+    vector<int> vec(n);
+    forn(i, m) {
+      cin >> u >> v;
+      u--, v--;
+      G[u].pb(v);
+    }
+    bfs(a);
+    if (!vis[b])cout << -1 << endl;
+    else cout << dist[b] << endl;
+    forn(i, n) G[i].clear();
+    fill(all(vis), 0);
+    fill(all(dist), 0);
+  }
+  return 0;
+}
+
--- fixeds/ejer.cpp
+++
@@ -1,1 +0,0 @@
-//code donde sus salidas sean de n.00000 que requiera usar  cout << fixed << setprecision(1) << ans << endl
