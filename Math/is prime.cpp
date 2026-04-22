309491d157593947e30c3a60dd2a11c1774b14d9
Karencitaing <karendaniela644@gmail.com>
2026-04-21 22:50:12 -0500

    Update is prime.cpp
    ㅤㅤ

--- Math/is prime.cpp
+++ Math/is prime.cpp
@@ -1,3 +1,4 @@
+//pregunta si es un numero primo o no(llamo la funcion con mi n);
 bool is_prime(int64_t n) {
     if (n < 2) return 0;
     for (int64_t p = 2; p * p <= n; p += p % 2 + 1) if (n % p == 0) return 0;
