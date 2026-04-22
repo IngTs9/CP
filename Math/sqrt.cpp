61098aef9781d42bb763d96d6abbef5e0c4a4a3b
Karencitaing <karendaniela644@gmail.com>
2026-04-21 22:47:58 -0500

    Update sqrt.cpp
    ㅤㅤ

--- Math/sqrt.cpp
+++ Math/sqrt.cpp
@@ -1,3 +1,4 @@
+//para encontrar la raiz cuadrada de un numero por si deja de funcionar la funcion original de c++. la respuesta es ans;
 ll int_sqrt (ll x) {
 	ll ans = 0;
 	for (ll k = 1LL << 30; k != 0; k /= 2)
