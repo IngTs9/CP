1a60fa835365e2b29c9bc4d01cd0829726862d1d
Karencitaing <karendaniela644@gmail.com>
2026-04-21 22:39:19 -0500

    Update Sieve.cpp
    ㅤㅤ

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -1,3 +1,7 @@
+//En bs marco cada posición con 0 o 1 para identificar si es un numero primo
+//(llamo la funcion y luego imprimo a bs en la posicion n).
+//En prime guardo todos primos que hay hasta ese número (llamo la funcion y luego coloco
+//mi n en el tamaño de la constante y luego ya imprimo  al vector.
 const int MAX = int(1e6);
 bitset<MAX + 5> bs;
 vector<int64_t> prime;
