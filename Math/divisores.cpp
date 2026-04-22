36cd0a6f7e4bbac520dc2e31f3c3579623b90c79
Karencitaing <karendaniela644@gmail.com>
2026-04-21 22:46:06 -0500

    Update divisores.cpp
    ㅤㅤ

--- Math/divisores.cpp
+++ Math/divisores.cpp
@@ -1,3 +1,6 @@
+//en ans se guardan los divisores de un n (en el main debo de crear otro
+//vector que va a ser igual a mi funcion dado un n, ejemplo: vector<int>divisores=div(n) 
+//y pues imprimo mi vector de divisores.
 vector<int>div(int n) {
     vector<int> ans;
     for (int i = 1; i * i <= n; i++) {
