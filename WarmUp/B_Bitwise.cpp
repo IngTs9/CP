e0978ae1a3c7f56c67d368d16e758c9cf20f747b
dyann <vcordobacorrales@gmail.com>
2026-04-27 11:23:46 -0500

    Refactor B_Bitwise.cpp for clarity and simplicity
    Removed unnecessary includes and macros, simplified main function structure.

--- WarmUp/B_Bitwise.cpp
+++ WarmUp/B_Bitwise.cpp
@@ -1,21 +1,3 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-#define pb push_back
-#define i insert
-#define sz(x) int((x).size())
-#define forn(i, n) for (int i = 0; i < n; i++)
-#define forna(i, x, n) for (int i = x; i <= n; i++)
-#define all(x) x.begin(), x.end()
-#define each(i, x) for (auto &&i : x)
-#define forinver(i, n) for (int i = n - 1; i >= 0; i--)
-using namespace std;
-#define int int64_t
-
-
-main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
   int t;
   cin>>t;
   string s,k;
@@ -29,8 +11,3 @@ main() {
     cout<<a<<" "<<b<<endl;
     
   }
-
-
-
-return 0;
-}
