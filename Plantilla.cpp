7c2bfb1a1aae9ea6fe6b3b0d67ffe7045d1bdac7
dyann <vcordobacorrales@gmail.com>
2026-05-04 06:43:20 -0500

    Create Plantilla.cpp

---
+++ Plantilla.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define i insert
+#define sz(x) x.size()
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define forna(i, x, n) for (int i = x; i <= n; i++)
+#define all(x) x.begin(), x.end()
+#define each(i, x) for (auto &&i : x)
+#define forinver(i, n) for (int i = n - 1; i >= 0; i--)
+using namespace std;
+#define int int64_t
+
+main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  
+    
+
+  return 0;
+}
+
